271eb1a2d9f938d34c592d82054ad6d0462a5bc3
Senderhauf <centerhauf@gmail.com>
2018-11-22 23:17:29 -0600

    write csv to usb

--- mpu6050_raspi/cutie.py
+++ mpu6050_raspi/cutie.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import sys
 
 import getpass
-from typing import List, Optional
+#from typing import List, Optional
 
 from colorama import init
 import readchar
