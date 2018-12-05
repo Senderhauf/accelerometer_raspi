b74d991bba34d8ab1ad62707ce34ca05df18138e
hartmut <hartmut@uwm.edu>
2018-12-05 17:38:57 -0600

    button dummy test

--- source/dummy.py
+++ source/dummy.py
@@ -2,28 +2,42 @@ from Adafruit_CharLCD import Adafruit_CharLCD
 
 from gpiozero import Button
 
+from time import sleep
+
 button_green = Button(2)
 button_red = Button(3)
 button_down = Button(4)
 button_up = Button(17)
 
+lcd = Adafruit_CharLCD()
 
-def get_button_status():
-    return {'up': button_up.is_pressed, 
-            'down': button_down.is_pressed, 
-            'green': button_green.is_pressed, 
-            'red': button_red.is_pressed}
+def button_up_pressed():
+	lcd.clear()
+	lcd.message('Button UP pressed')
+	sleep(1)
+
+def button_down_pressed():
+	lcd.clear()
+	lcd.message('Button DOWN pressed')
+	sleep(1)
+
+def button_red_pressed():
+	lcd.clear()
+	lcd.message('Button RED pressed')
+	sleep(1)
+
+def button_green_pressed():
+	lcd.clear()
+	lcd.message('Button GREEN pressed')
+	sleep(1)
 
 if __name__ == "__main__":
 	
-	lcd = Adafruit_CharLCD()
-	lcd.clear()
+	button_up.when_pressed = button_up_pressed
+	button_down.when_pressed = button_down_pressed
+	button_red.when_pressed = button_red_pressed
+	button_green.when_pressed = button_green_pressed
 	
 	while True:
-		button_status = get_button_status()
-		if button_status['up']:
-			lcd.message('Button status is UP')
-		elif button_status['down']:
-			lcd.message('Button status is DOWN')
-		else:
-			lcd.message('Button status is UKNOWN')
+		lcd.clear()
+		lcd.message('Waiting for Button')
