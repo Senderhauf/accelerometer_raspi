7de16453f31f918fa08f61501e3fe83de65be5e1
hartmut <hartmut@uwm.edu>
2018-12-05 19:51:52 -0600

    add button functionality to cutie

--- source/__init__.py
+++ source/__init__.py
@@ -1,6 +1,8 @@
 
 from record_data import record_data
 from transfer_usb import transfer_usb
+from Adafruit_CharLCD import Adafruit_CharLCD
+
 import cutie
 
 def main():
@@ -13,13 +15,20 @@ def main():
             'RECORD DATA',
             'TRANSFER DATA']    
 
-        selected_option = cutie.select(options, selected_index=0)
+        try:
+            selected_option = cutie.select(options, selected_index=0)
 
-        if selected_option == 0:
-            record_data()
-        elif selected_option == 1:
-            transfer_usb()
+            if selected_option == 0:
+                record_data()
+            elif selected_option == 1:
+                transfer_usb()
+        except:
+            continue
 
+        finally:
+            lcd = Adafruit_CharLCD()
+            lcd.message('Error. Restart Module.')
+            lcd.clear()
 
 if __name__ == '__main__':
     main()
