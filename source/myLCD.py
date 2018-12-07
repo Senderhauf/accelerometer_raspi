4c18b9d488f579e5d2b52044acb82c459e9ae878
Senderhauf <centerhauf@gmail.com>
2018-12-06 23:41:57 -0600

    myLCD

--- source/myLCD.py
+++ source/myLCD.py
@@ -33,14 +33,14 @@ import os.path
 ### The number are the pin numbers of the Raspberry Pi, not the GPIO numbers.
 ### If using a older Raspberry Pi with only 26 pins make sure you have the correct pin pinnumbers.
 
-GPIO_PIN_RS = 37
+GPIO_PIN_RS = 26
 GPIO_PIN_RW = None ## Raspberry Pi cannot handle if the display writes data. Could damage RPi. This pin on the LCD was connected to gound.
-GPIO_PIN_E_TOP = 35
-GPIO_PIN_E_BOTTOM = 40
-GPIO_PIN_D4 = 33
-GPIO_PIN_D5 = 31
-GPIO_PIN_D6 = 29
-GPIO_PIN_D7 = 23
+GPIO_PIN_E_TOP = 19
+GPIO_PIN_E_BOTTOM = 21
+GPIO_PIN_D4 = 13
+GPIO_PIN_D5 = 6
+GPIO_PIN_D6 = 5
+GPIO_PIN_D7 = 11
 LCD_COLUMNS = 40
 LCD_ROWS = 2
 LCD_DOT_SIZE = 8
@@ -48,8 +48,8 @@ LCD_DOT_SIZE = 8
 LCD_BRIGHTNESS = 0 # to be used with PWM for control of the LCD brightness.
 
 ### Initialize the LCD
-lcd_top = CharLCD(pin_rs=GPIO_PIN_RS, pin_rw=GPIO_PIN_RW,  pin_e=GPIO_PIN_E_TOP, pins_data=[GPIO_PIN_D4, GPIO_PIN_D5, GPIO_PIN_D6, GPIO_PIN_D7], numbering_mode=GPIO.BOARD, cols=LCD_COLUMNS, rows=LCD_ROWS, dotsize=LCD_DOT_SIZE)
-lcd_bottom = CharLCD(pin_rs=GPIO_PIN_RS, pin_rw=GPIO_PIN_RW, pin_e=GPIO_PIN_E_BOTTOM, pins_data=[GPIO_PIN_D4, GPIO_PIN_D5, GPIO_PIN_D6, GPIO_PIN_D7], numbering_mode=GPIO.BOARD, cols=LCD_COLUMNS, rows=LCD_ROWS, dotsize=LCD_DOT_SIZE)
+lcd_top = CharLCD(pin_rs=GPIO_PIN_RS, pin_rw=GPIO_PIN_RW,  pin_e=GPIO_PIN_E_TOP, pins_data=[GPIO_PIN_D4, GPIO_PIN_D5, GPIO_PIN_D6, GPIO_PIN_D7], numbering_mode=GPIO.BCM, cols=LCD_COLUMNS, rows=LCD_ROWS, dotsize=LCD_DOT_SIZE)
+lcd_bottom = CharLCD(pin_rs=GPIO_PIN_RS, pin_rw=GPIO_PIN_RW, pin_e=GPIO_PIN_E_BOTTOM, pins_data=[GPIO_PIN_D4, GPIO_PIN_D5, GPIO_PIN_D6, GPIO_PIN_D7], numbering_mode=GPIO.BCM, cols=LCD_COLUMNS, rows=LCD_ROWS, dotsize=LCD_DOT_SIZE)
 
 var = 1
 i = 0
